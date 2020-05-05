254f76f8df3660156c395e970d8d4d7219d6e57f
stlngds <64813108+stlngds@users.noreply.github.com>
2020-05-04 21:29:40 -0800

    Sync

--- unison/src/unison/UniMain.java
+++ unison/src/unison/UniMain.java
@@ -1,5 +1,33 @@
+/*
+ * Greyson Biggs
+ * Clinton Jeffery
+ * CS 210
+ * 5/8/2020
+ * HW #5 - Unicon class-to-JSON Java parser
+ */
+
 package unison;
 
 public class UniMain {
-
+	public static void main(String args[]) {
+		/*
+		 * Parses Unicon classes to JSON
+		 * Will wig out if it runs into syntax errors
+		 * In JSON format, identify classes, superclasses, fields, and methods and their args
+		 * e.g. 
+		 * { "class": "A",
+		 * 	"super": ["B"],
+		 * 	"fields": ["x", "y", "z"],
+		 * 	"methods": {
+		 * 		"equals": ["other", "seen"],
+		 * 		"hash_code": ["depth", "seen"]
+		 * 	}
+		 * }
+		 * 
+		 * Takes multiple .icn files as input, outputs JSON to uniuml.json
+		 * 
+		 * Unicon class syntax (things enclosed in asterisks are non-literal):
+		 * 	
+		 */
+	}
 }
